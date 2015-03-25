213f264d4f8821ebbb3191cb2f4f2ba4c8fceaf6
Sam Dindyal <sam.dindyal@icloud.com>
2015-03-25 12:48:10 -0400

    Added title panel for UI

---
+++ TitlePanel.java
@@ -0,0 +1,51 @@
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.BorderLayout;
+
+public class TitlePanel extends JPanel
+{
+	private JLabel title, panelText, panelImage;
+	private JPanel textPanel, redStripe;
+	private String currentPanel;
+
+	private Font titleFont, bodyTextFont;
+
+	public TitlePanel(String currentPanel)
+	{
+		setBackground(Color.WHITE);
+		setBorder(new EmptyBorder(10, 10, 10, 10) );
+		setLayout(new BorderLayout());
+
+		panelImage = new JLabel(new ImageIcon("res/bug.png"));
+
+		redStripe = new JPanel();
+		redStripe.setBackground(Color.RED);
+
+		titleFont 		= new Font ("Arial", Font.BOLD, 36);
+		bodyTextFont 	= new Font("Arial", Font.PLAIN, 14);
+
+		title 		= new JLabel("Bug Report System (BRS)", SwingConstants.RIGHT);
+		panelText 	= new JLabel(currentPanel, SwingConstants.RIGHT);
+
+		textPanel = new JPanel();
+		textPanel.setBackground(Color.WHITE);
+		textPanel.setLayout(new BorderLayout());
+
+		title.setFont(titleFont);
+		panelText.setFont(bodyTextFont);
+
+		textPanel.add(title, BorderLayout.CENTER);
+		textPanel.add(panelText, BorderLayout.AFTER_LAST_LINE);
+
+		add(textPanel, BorderLayout.CENTER);
+		add(panelImage, BorderLayout.WEST);
+		add(redStripe, BorderLayout.SOUTH);
+
+	}
+}
--- UserInterface.java
+++ UserInterface.java
@@ -1,16 +1,29 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+import java.awt.Color;
+import java.awt.BorderLayout;
 
 public class UserInterface extends JFrame
 {
+	private JPanel titlePanel;
 	public UserInterface()
 	{
 		setSize(720, 480);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setBackground(Color.WHITE);
+
+		setLayout(new BorderLayout());
+
+		titlePanel = new TitlePanel("login");
+
+		add(titlePanel, BorderLayout.NORTH);
 
 
 		setVisible(true);
 	}
+
+
 }
