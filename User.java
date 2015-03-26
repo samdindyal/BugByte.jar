260e1d2850dc0c7caa59b0d4059d322a8ea4a12c
Sam Dindyal <sam.dindyal@icloud.com>
2015-03-26 10:13:57 -0400

    Added more fields for the user

--- User.java
+++ User.java
@@ -1,18 +1,30 @@
 import java.io.Serializable;
 
+import java.util.HashSet;
+import java.util.UUID;
+
 public class User implements Serializable
 {
-	private String username, password;
+	private String 			username, password, firstName, lastName, emailAddress;
+	private HashSet<UUID> 	keys;
 
-	public User(String username, String password)
+	public User(String username, String password, String firstName, String lastName, String emailAddress)
 	{
-		this.username = username;
-		this.password = password;
+		this.username 		= username;
+		this.password 		= password;
+		this.firstName 		= firstName;
+		this.lastName		= lastName;
+		this.emailAddress 	= emailAddress;
+
+		keys = new HashSet<UUID>();
 	}
 
 	public User()
 	{
-		username = "";
-		password = "";
+		username 		= "";
+		password 		= "";
+		firstName 		= "";
+		lastName		= "";
+		emailaddress	= "";
 	}
 }
