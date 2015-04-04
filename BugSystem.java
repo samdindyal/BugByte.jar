91f42c3dc390d874a08cc191a902bbe8e1796d7a
Samuel Dindyal <sam.dindyal@icloud.com>
2015-04-04 16:20:01 -0400

    Cleaned up a bit

--- BugSystem.java
+++ BugSystem.java
@@ -28,9 +28,7 @@ public class BugSystem implements Serializable
 	public BugSystem(String directory)
 	{
 		this();
-
 		file = new File(directory);
-
 		if (!loadFromDisk())
 				writeToDisk();
 	}
