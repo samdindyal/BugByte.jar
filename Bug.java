5743419cb06fb0275b6d9f598cb898eb0bd8a44f
Samuel Dindyal <sam.dindyal@icloud.com>
2015-04-05 16:11:36 -0400

    Added date modified and date created variables

--- Bug.java
+++ Bug.java
@@ -1,9 +1,15 @@
+import java.util.Date;
+
 public class Bug
 {
 	private String			ID;
 	private BugStatus 		status;
 	private String 			description;
 	private BugPriority 	priority;
+	private Date 			dateModified;
+
+	public static final Date DATE_CREATED = new Date();
+
 
 	public Bug(BugStatus status, BugPriority priority, String description, String ID)
 	{
@@ -11,13 +17,28 @@ public class Bug
 		this.priority 		= priority;
 		this.description 	= description;
 		this.ID 			= ID;
+		dateModified 		= new Date();
 	}
 
 	public BugStatus getStatus(){return status;}
 	public String getDescription(){return description;}
 	public BugPriority getPriority(){return priority;}
+	public Date getDateModifed(){return dateModified;}
+	public Date getDateCreated(){return DATE_CREATED;}
 
-	public void setStatus(BugStatus newStatus){status = newStatus;}
-	public void setPriority(BugPriority newPriority){priority = newPriority;}
-	public void setDescription(String newDescription){description = newDescription;}
+	public void setStatus(BugStatus newStatus)
+	{
+		status = newStatus;
+		dateModified = new Date();
+	}
+	public void setPriority(BugPriority newPriority)
+	{
+		priority = newPriority;
+		dateModified = new Date();
+	}
+	public void setDescription(String newDescription)
+	{
+		description = newDescription;
+		dateModified = new Date();
+	}
 }
