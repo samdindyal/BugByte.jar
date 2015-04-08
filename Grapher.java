aca04fdfdd4f889613a17416bb85df97ee933bcd
Samuel Dindyal <sam.dindyal@icloud.com>
2015-04-08 10:32:10 -0400

    Simple graph panel

---
+++ Grapher.java
@@ -0,0 +1,103 @@
+import javax.swing.JScrollPane;
+import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import java.awt.geom.Line2D;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Grapher extends JPanel
+{
+
+	private Line2D 				x_axis, y_axis;
+	private int 				margin, horizontalSpacing, verticalSpacing;
+	private Color 				foreground, background;
+	private Trend 				trend;
+	private ArrayList<Double> 	list;
+	private double 				scale, numericalVerticalSpacing;
+	private int[]				xAxisValues, yAxisValues;
+
+	public Grapher(Color foreground, Color background, int margin, ArrayList<Double> list)
+	{
+		super.setSize(500, 500);
+		this.background = background;
+		this.foreground = foreground;
+		this.margin 	= margin;
+		this.list 		= list;
+		
+		calculateNumericalVerticalSpacing();
+		calculateXAxis();
+		calculateYAxis();
+		calculateHorizontalSpacing();
+		calculateVerticalSpacing();
+
+		trend = new Trend(Color.GREEN, list, margin, 500, 500);
+
+		x_axis = new Line2D.Double(margin, getHeight()-margin, getWidth(), getHeight()-margin);
+		y_axis = new Line2D.Double(margin, 0, margin, getHeight()-margin);
+	}
+
+	@Override
+	public void setSize(int x, int y)
+	{
+		super.setSize(x,y);
+		calculateXAxis();
+		calculateYAxis();
+		calculateHorizontalSpacing();
+		calculateVerticalSpacing();
+	}
+
+	public void paintComponent(Graphics g)
+	{
+		g.setColor(background);
+		g.fillRect(0, 0, getWidth(), getHeight());
+
+		Graphics2D g2 = (Graphics2D)g;
+		g2.setColor(foreground);
+		g2.draw(x_axis);
+		g2.draw(y_axis);
+
+		for (int i = 0; i < list.size(); i++)
+			g.drawString(xAxisValues[i] + "", margin + (horizontalSpacing * i), getHeight()-margin + 50);
+		for (int i = 0; i < yAxisValues.length; i++)
+			g.drawString(yAxisValues[i] + "", margin - 50, getHeight() - margin - (verticalSpacing*i));
+
+		trend.draw(g2);
+	}	
+
+	public void calculateXAxis()
+	{
+		xAxisValues = new int[list.size()];
+		for (int i = 0; i < xAxisValues.length; i++)
+			xAxisValues[i] = i;
+	}
+
+	public void calculateYAxis()
+	{
+		yAxisValues = new int [5];
+		for (int i = 0; i < yAxisValues.length; i++)
+			yAxisValues[i] = (int)(i*numericalVerticalSpacing);
+	}
+
+	public void calculateScale(){scale = (getHeight() - margin)/Collections.max(list);}
+	public void calculateHorizontalSpacing(){horizontalSpacing = (getWidth() - margin)/list.size();}
+	public void calculateVerticalSpacing(){verticalSpacing = (getHeight()-margin)/5;}
+	public void calculateNumericalVerticalSpacing(){numericalVerticalSpacing = ((Collections.max(list) - Collections.min(list))/list.size());}
+
+	//Self-testing main
+	public static void main (String[] args)
+	{
+		ArrayList<Double> list = new ArrayList<Double>();
+		list.add(20.0);
+		list.add(30.0);
+		list.add(20.0);
+		list.add(10.0);
+		list.add(40.0);		
+
+		new ComponentViewer(new Grapher(Color.WHITE, Color.DARK_GRAY.darker(), 75, list));
+	}
+}
---
+++ Trend.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.geom.Line2D;
+import java.awt.geom.Ellipse2D;
+
+public class Trend
+{
+	private Color 				colour;
+	private ArrayList<Double> 	yCoordinates;
+	private int					margin, horizontalLimit, verticalLimit, spacing;
+	private Line2D.Double		line;
+	private double 				scale;
+
+	public Trend(Color colour, ArrayList<Double> yCoordinates, int margin, int horizontalLimit, int verticalLimit)
+	{
+		this.colour 			= colour;
+		this.yCoordinates 		= yCoordinates;
+		this.margin 			= margin;
+		this.horizontalLimit	= horizontalLimit;
+		this.verticalLimit		= verticalLimit;
+		
+		calculateScale();
+		calculateSpacing();
+
+	}
+
+	public void calculateScale()
+	{
+		scale = (verticalLimit - margin)/Collections.max(yCoordinates);
+	}
+	public void calculateSpacing()
+	{
+		spacing = (horizontalLimit - margin)/yCoordinates.size();
+	}
+
+	public void draw(Graphics2D g2)
+	{
+		int x, y;
+		x = y = 0;
+
+		g2.setColor(colour);
+
+		for (int i = 0; i < yCoordinates.size() - 1; i++)
+		{
+			line = new Line2D.Double(margin + (spacing * i), verticalLimit - margin - (yCoordinates.get(i)*scale),
+										margin + (spacing * (i+1)), (verticalLimit - margin -(yCoordinates.get(i+1))*scale));
+			g2.fill(new Ellipse2D.Double(margin + (spacing * i)-5, (verticalLimit - margin - (yCoordinates.get(i))*scale)-5, 10, 10));
+			g2.fill(new Ellipse2D.Double(margin + (spacing * (i+1))-5, verticalLimit - margin - (yCoordinates.get(i+1)*scale)-5,10, 10));
+			g2.draw(line);
+		}
+
+	}
+
+
+}
