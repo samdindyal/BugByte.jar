a79dc053c67483f2691536939f907a61b274b1cb
Samuel Dindyal <sam.dindyal@icloud.com>
2015-04-08 06:08:51 -0400

    Replaced accessor methods with public variables

--- BugStatus.java
+++ BugStatus.java
@@ -5,22 +5,12 @@ public enum BugStatus implements Serializable
 	NOT_FIXED(0,"The bug has not been fixed."),
 	FIXED(1, "The bug has been fixed.");
 
-	private final int 		status;
+	public final int 		status;
 	private final String 	statusDescription;
 
 	BugStatus(int status, String statusDescription)
 	{
 		this.status 			= status;
 		this.statusDescription 	= statusDescription;
-	}
-
-	public int getStatus()
-	{
-		return status;
-	}
-
-	public String getDescription()
-	{
-		return statusDescription;
 	}
 }
