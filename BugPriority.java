f804ef89dba87e73a46ca6b7fb6a2b10d44867e8
Samuel Dindyal <sam.dindyal@icloud.com>
2015-04-08 07:34:45 -0400

    Changed numerical values of priorities

--- BugPriority.java
+++ BugPriority.java
@@ -2,9 +2,9 @@ import java.io.Serializable;
 
 public enum BugPriority implements Serializable
 {
-	LOW(1,"Low"),
-	MEDIUM(2,"Medium"),
-	HIGH(3,"High");
+	LOW(0,"Low"),
+	MEDIUM(1,"Medium"),
+	HIGH(2,"High");
 
 	public final int		priority;
 	public final String	description;
